1424d57d2f6d46e56f86c616c28807e6bac53f02
alanssitis <alanchunggt@gmail.com>
2023-08-27 12:45:57 -0400

    use Filename module

--- lib/common.ml
+++ lib/common.ml
@@ -1,5 +1,8 @@
-let root = "./.classorg_root"
-let archive = "./archive/.classorg_archive"
+open Filename
+
+let root_marker = ".classorg_root"
+let archive_dir = concat current_dir_name "archive"
+let archive_marker = concat archive_dir ".classorg_archive"
 
 let touch_empty_file filename =
   let oc = open_out filename in
--- lib/initialize.ml
+++ lib/initialize.ml
@@ -3,8 +3,8 @@ open Common
 exception InitializeException of string
 
 let is_initialized () =
-  let root_exists = Sys.file_exists root in
-  let archive_exists = Sys.file_exists archive in
+  let root_exists = Sys.file_exists root_marker in
+  let archive_exists = Sys.file_exists archive_marker in
   if root_exists <> archive_exists then
     raise
       (InitializeException
@@ -18,10 +18,9 @@ let main () =
         Logs.warn (fun m ->
             m "classorg was already initialized in current directory")
     | false ->
-        touch_empty_file root;
-        let archive_dir = "./archive" in
+        touch_empty_file root_marker;
         if not (Sys.file_exists archive_dir) then Sys.mkdir archive_dir 509;
-        touch_empty_file archive
+        touch_empty_file archive_marker
   with e ->
     let msg = Printexc.to_string e in
     Logs.err (fun m -> m "Failed in initialize: %s" msg)
