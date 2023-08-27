e8492434605251c327db9a9710c1a4ff9fbbf112
alanssitis <alanchunggt@gmail.com>
2023-08-27 13:44:13 -0400

    progress on create commands

--- lib/commands.ml
+++ lib/commands.ml
@@ -1,5 +1,11 @@
 let placeholder () = Logs.err (fun m -> m "Not implemented yet")
 
+let create_class class_name () =
+  Logs.err (fun m -> m "Print smtg %s" class_name)
+
+let create_homework class_name homework () =
+  Logs.err (fun m -> m "Print smtg %s %s" class_name homework)
+
 let setup_log style_renderer level =
   Fmt_tty.setup_std_outputs ?style_renderer ();
   Logs.set_level level;
@@ -11,6 +17,14 @@ open Cmdliner
 let setup_log =
   Term.(const setup_log $ Fmt_cli.style_renderer () $ Logs_cli.level ())
 
+let class_name =
+  let doc = "Name of the class" in
+  Arg.(required & pos 0 (Arg.some Arg.string) None & info [] ~doc ~docv:"CLASS")
+
+let homework =
+  let doc = "Homework number" in
+  Arg.(required & pos 1 (Arg.some Arg.string) None & info [] ~doc ~docv:"HOMEWORK")
+
 let archive_cmd =
   let doc = "Archive classes." in
   let info = Cmd.info "archive" ~doc in
@@ -19,12 +33,12 @@ let archive_cmd =
 let create_class_cmd =
   let doc = "Create a new class workspace." in
   let info = Cmd.info "class" ~doc in
-  Cmd.v info Term.(const placeholder $ setup_log)
+  Cmd.v info Term.(const create_class $ class_name $ setup_log)
 
 let create_homework_cmd =
-  let doc = "Create a new homework space." in
+  let doc = "Create a new homework space. Default is a LaTeX assignment." in
   let info = Cmd.info "homework" ~doc in
-  Cmd.v info Term.(const placeholder $ setup_log)
+  Cmd.v info Term.(const create_homework $ class_name $ homework $ setup_log)
 
 let create_cmd =
   let doc = "Create new workspace or homeworks." in
