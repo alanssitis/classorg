95586135c4029b143fc9e66f4c78d1187d58f040
alanssitis <alanchunggt@gmail.com>
2023-08-27 12:19:40 -0400

    updated documentation in help

--- lib/commands.ml
+++ lib/commands.ml
@@ -12,36 +12,36 @@ let setup_log =
   Term.(const setup_log $ Fmt_cli.style_renderer () $ Logs_cli.level ())
 
 let archive_cmd =
-  let doc = "archive doc" in
+  let doc = "Archive classes." in
   let info = Cmd.info "archive" ~doc in
   Cmd.v info Term.(const placeholder $ setup_log)
 
 let create_class_cmd =
-  let doc = "create class doc" in
+  let doc = "Create a new class workspace." in
   let info = Cmd.info "class" ~doc in
   Cmd.v info Term.(const placeholder $ setup_log)
 
 let create_homework_cmd =
-  let doc = "create homework doc" in
+  let doc = "Create a new homework space." in
   let info = Cmd.info "homework" ~doc in
   Cmd.v info Term.(const placeholder $ setup_log)
 
 let create_cmd =
-  let doc = "create doc" in
+  let doc = "Create new workspace or homeworks." in
   let info = Cmd.info "create" ~doc in
   Cmd.group info [ create_class_cmd; create_homework_cmd ]
 
 let init_cmd =
-  let doc = "init doc" in
+  let doc = "Initialize current directory with classorg." in
   let info = Cmd.info "init" ~doc in
   Cmd.v info Term.(const Initialize.main $ setup_log)
 
 let list_cmd =
-  let doc = "list doc" in
+  let doc = "List all current active classes." in
   let info = Cmd.info "list" ~doc in
   Cmd.v info Term.(const placeholder $ setup_log)
 
 let cmd =
-  let doc = "main doc" in
+  let doc = "An opinionated class assignment and directory management CLI." in
   let info = Cmd.info "classorg" ~version:"0.1" ~doc in
   Cmd.group info [ archive_cmd; create_cmd; init_cmd; list_cmd ]
